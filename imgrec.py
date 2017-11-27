7499c2ce8ba6a7b1f3e3327fa2eba72f83f5439e
joljols <j.w.jolles@gmail.com>
2017-11-28 00:26:49 +0100

    as

--- imgrec.py
+++ imgrec.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
 #!/usr/bin/python
 
 #######################################
@@ -8,102 +14,126 @@
 #######################################
 
 # Load modules
-import picamera
-import time
-import socket
+from picamera import PiCamera
+from time import sleep, strftime
+from datetime.datetime import now
+from socket import gethostname
 import os
-import subprocess
-from fractions import Fraction
 
 # Define recording function
-def record(duration = 10, resolution = (1000,1000),
-           shutterspeed = 10000, compensation = 0,
-           framerate = 0.2, sharpness = 50, iso = 200,
-           contrast = 20, brightness = 40, saturation = -100,
-           quality = 15):
-
+def record(resolution = (1000,1000),
+           compensation = 0,
+           shutterspeed = 10000,
+           iso = 200,
+           brightness = 40,
+           sharpness = 50,
+           contrast = 20,
+           saturation = -100,
+           quality = 20,
+           wait = 5.0):
+    
     """
         Run automated video recording with the rpi camera
         
+        Storage location
+        ----------
+        The folder where the images will be stored is automatically
+        set to the folder on the server that reflects the rpi name,
+        for example /home/pi/SERVER/pi41.
+        
         Parameters
         ----------
-        location : str, default = "NAS"
-            Location where images will be stored. This is automatically
-            set to the server folder reflecting the rpi name.
-        duration : int, default = 60
-            Total duration during which images will be recorded
-        resolution : tuple, default = (1000,1000)
-            The width and height of the images to be recorded.
-        framerate : int or fraction, default = Fraction(1,6)
-            The framerate at which images should be taken. The minimum
-            framerate is 6s, which is provided as the default.
-        shutterspeed : int, detault = 10000
-            Shutter speed of the camera in microseconds. Thus the
-            default of 10000 is equivalent to 1/100th of a second
+        resolution : tuple, default = (1000, 1000)
+            The width and height of the images that will be recorded.
         compensation : int, default = 0
             Camera lighting compensation. Ranges between 0 and 20.
             Compensation artificially adds extra light to the image.
-        sharpness : int, default = 50
-            The sharpness of the image. Valid values are between
-            -100 and 100.
+        shutterspeed : int, detault = 10000
+            Shutter speed of the camera in microseconds, i.e. the
+            default of 10000 is equivalent to 1/100th of a second. A
+            longer shutterspeed will result in a brighter image but
+            more likely motion blur.
         iso : int, default = 200
             The camera iso value. Higher values are more light
             sensitive but have higher gain. Valid values are
-            between 0 (auto) and 1600.
-        contrast : int, default = 15
-            The camera contrast. Valid values are between -100
-            and 100.
+            between 200 and 1600.
         brightness : int, default = 55
             The brightness level of the camera. Valid values are
             between 0 and 100.
+        sharpness : int, default = 50
+            The sharpness of the image, an integer value between -100
+            and 100.
+        contrast : int, default = 20
+            The image contrast, an integer value between 0 and 100.
         saturation : int, default -100
-            The color saturation level of the camera. Valid values
-            are between -100 and 100.
-        quality : int, default 18
-            Specifies the quality that the encoder should attempt
-            to maintain. Valid values are between 10 and 40, where
-            10 is extremely high quality, and 40 is extremely low.
-            
+            The color saturation level of the image, an integer
+            value between -100 and 100.
+        quality : int, default = 20
+            Defines the quality of the JPEG encoder as an integer
+            ranging from 1 to 100. Defaults to 20.
+        wait : float, default = 5.0
+            The delay between subsequent images in seconds. When a 
+            delay is provided that is less than (shutterspeed + 
+            processing time) delay will be automatically set at 0 
+            and images thus taken continuously.
+        
         Output
         -------
-        a h264 video
+        A series of JPEG images, automatically named based on 
+        the rpi number, date, and time, following a standard 
+        naming convention, e.g. pi11_172511_im00010_153012.jpg
         
-    """
-
-    # Set directory to right location
+        """
+    
+    print "=================================================="
+    print strftime("ImgRec started: Date: %y/%m/%d; Time: %H:%M:%S")
+    print "=================================================="
+    
+    # Acquire rpi name
+    rpi = gethostname()
+    
+    # Set the directory
     server = "/home/pi/SERVER/"
-    rpi = socket.gethostname()
-    foldername = server+rpi
-    if os.path.exists(foldername):
-        os.chdir(foldername)
+    location = server + rpi
+    if os.path.exists(location):
+        os.chdir(location)
+    
+    # Set-up automatic filenaming
+    daystamp = "_{timestamp:%Y%m%d}"
+    counter = "_im{counter:05d}"
+    timestamp = "_{timestamp:%H%M%S}"
+    ftype = ".jpg"
+    filename = rpi+daystamp+counter+timestamp+ftype
 
-    # Get the current date and time
-    date = time.strftime("%y%m%d_%H%M%S")
-
-    # Create the video filename
-    filename = rpi+"_"+date+".h264"
-
-    print(time.strftime("%H:%M:%S")+" - recording video "+filename)
-
-    # Now record the video
-    with picamera.PiCamera() as camera:
-        camera.resolution = resolution
-        camera.exposure_compensation = compensation
-        camera.framerate = framerate
-        time.sleep(1)
-        camera.exposure_mode = 'off'
-        camera.awb_mode = 'off'
-        camera.sharpness = sharpness
-        camera.shutter_speed = shutterspeed
-        camera.iso = iso
-        camera.contrast = contrast
-        camera.saturation = saturation
-        camera.brightness = brightness
-        camera.start_recording(filename, quality = quality)
-        camera.wait_recording(duration-1)
-        camera.stop_recording()
-
-    print(time.strftime("%H:%M:%S")+" - finished video "+filename)
+    # Set-up the camera with the right parameters
+    camera = PiCamera() as camera:
+    camera.resolution = resolution
+    camera.exposure_compensation = compensation
+    sleep(0.1)
+    camera.exposure_mode = 'off'
+    camera.awb_mode = 'off'
+    camera.shutter_speed = shutterspeed
+    camera.sharpness = sharpness
+    camera.iso = iso
+    camera.contrast = contrast
+    camera.saturation = saturation
+    camera.brightness = brightness
+    
+    # Start taking images
+    bef = now()
+    for i, img in enumerate(camera.capture_continuous(filename, 
+                                 format="jpeg", quality=quality):
+        if i == 10:
+            break
+        delay = wait-(now()-bef).total_seconds()
+        print strftime("[%H:%M:%S] [rpi] - captured image "+img+"; next img in: "+str(round(delay,2))+"sec"
+        sleep(delay)
+        bef = now()
+    
+    print "=================================================="
+    print strftime("ImgRec stopped: Date: %y/%m/%d; Time: %H:%M:%S")
+    print "=================================================="
 
 # Run recording function
 record()
+
