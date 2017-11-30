bd015bbb6523e2648ef3d11ef51dd6d01bc6cded
joljols <j.w.jolles@gmail.com>
2017-12-01 00:19:55 +0100

    123dasd

--- imgrec.py
+++ imgrec.py
@@ -137,17 +137,17 @@ def record(imgwait = 5.0,
     camera.brightness = brightness
     
     # start taking images
-    bef = dt.now()
-    print bef
+    #bef = dt.now()
+    #print bef
     for i, img in enumerate(camera.capture_continuous(filename, format="jpeg", quality=quality)):
         if i == imgnr:
             break
-        delay = imgwait-(dt.now()-bef).total_seconds()
-        print delay
-        delay = 0 if delay < 0 else delay
-        print strftime("[%H:%M:%S][") + rpi + "] - captured " + img +               ", sleeping " + str(round(delay,2)) + "s.."
-        sleep(delay)
-        bef = dt.now()
+        #delay = imgwait-(dt.now()-bef).total_seconds()
+        #delay = 0 if delay < 0 else delay
+        #print strftime("[%H:%M:%S][") + rpi + "] - captured " + img + \
+        #      ", sleeping " + str(round(delay,2)) + "s.."
+        sleep(3)
+        #bef = dt.now()
     
     print "=================================================="
     print strftime("imgrec stopped: Date: %y/%m/%d; Time: %H:%M:%S")
