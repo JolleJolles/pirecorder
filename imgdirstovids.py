2618bcb07ca1c57f33de70c3669f9051ab12427b
joljols <j.w.jolles@gmail.com>
2018-07-26 14:35:14 +0200

    dd

--- imgdirstovids.py
+++ imgdirstovids.py
@@ -4,9 +4,10 @@
 # In[1]:
 
 
+
 #!/usr/bin/env python2.7
 # author JW Jolles
-# Last updated: 23 July 2018
+# Last updated: 25 July 2018
 
 # Import libraries
 #-----------------------
@@ -65,6 +66,8 @@ def dirconvert(maindir = ".", viddirname = "videos"):
     
     # Get list of all rpi directories
     pidirs = listfiles(dirs=True) if selectlist == "" else [selectlist]
+    
+    pidirs = [i for i in pidirs if i != exclude]
 
     # Create directory for videos
     createdir(viddirname)
@@ -96,13 +99,19 @@ def dirconvert(maindir = ".", viddirname = "videos"):
             vidname = subviddir + "/" + pidir + "_" + subdir + ".mp4"
             if iswin():
                 output = subprocess.check_output("(for %i in (*.jpg) do @echo file '%i')|sort /o imglist.txt", shell=True)
-                output = subprocess.check_output("ffmpeg -f concat -i imglist.txt -c:v libx264 -pix_fmt yuv420p -hide_banner -nostats -loglevel quiet -y " + vidname, shell=False)
+                if not imglistonly:
+                    output = subprocess.check_output("ffmpeg -f concat -i imglist.txt -c:v libx264 -pix_fmt yuv420p -hide_banner -nostats -loglevel quiet -y " + vidname, shell=False)
+                else:
+                    timediff = time() - t1
+                    print "Imglist created in "+"%.2f" % timediff+" sec.."
+                    
             else:
                 bashCommand = ['bash','-c', 'ffmpeg -pattern_type glob -i "*.jpg" -c:v libx264 -pix_fmt yuv420p -hide_banner -nostats -loglevel quiet -y ' + vidname]
                 output = subprocess.check_output(bashCommand)
             
-            timediff = time() - t1
-            print "Video created in "+"%.2f" % timediff+" sec.."
+            if not imglistonly:
+                timediff = time() - t1
+                print "Video created in "+"%.2f" % timediff+" sec.."
 
         print ""
 
@@ -118,17 +127,16 @@ else:
     ap = argparse.ArgumentParser()
     ap.add_argument("-d", "--dir", default="", help="video directory")
     ap.add_argument("-s", "--select", default="", help="selection of sub directories")
+    ap.add_argument("-e", "--exclude", default="", help="exclude directories")
+    ap.add_argument("-i", "--imglistonly", default=False, help="only create imglist or not")
     args = vars(ap.parse_args())
     filedir = args["dir"]
     selectlist = args["select"]
+    exclude = args["exclude"]
+    imglistonly = args["imglistonly"]
 
 
 # Start convert script
 #-----------------------
 dirconvert(filedir)
-
-
-# In[ ]:
-
-
 
