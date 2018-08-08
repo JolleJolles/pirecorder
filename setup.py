e357b0d249c5be6c5eda46a017e534da01f429d6
joljols <j.w.jolles@gmail.com>
2018-08-08 15:29:00 +0200

    change in structure

---
+++ setup.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2015-2018 Jolle Jolles <j.w.jolles@gmail.com>
+
+from setuptools import setup
+
+def check_dependencies():
+    install_requires = []
+
+    # Make sure dependencies exist
+    try:
+        import picamera
+    except ImportError:
+        install_requires.append('picamera')
+    try:
+        import socket
+    except ImportError:
+        install_requires.append('socket')
+    try:
+        import yaml
+    except ImportError:
+        install_requires.append('yaml')
+
+    dependency_links = ['http://github.com/jolleslab/AnimRec/tarball/']
+
+    return install_requires, dependency_links    
+
+
+if __name__ == "__main__":
+
+    install_requires = check_dependencies()
+
+    setup(name='animtrack',
+          author='Jolle Jolles',
+          author_email='j.w.jolles@gmail.com',
+          description='AnimTrack: behavioural tracking of free-moving animals',
+          url='http://jollejolles.com',
+          download_url='https://github.com/jolleslab/AnimTrack',
+          version=ATversion,
+          install_requires=install_requires,
+          dependency_links=dependency_links,
+          packages=['animtrack'])
