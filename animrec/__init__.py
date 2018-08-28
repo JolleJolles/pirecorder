50279963f0f853037b11cdedfd11ceb437c52bc0
joljols <j.w.jolles@gmail.com>
2018-08-28 12:00:11 +0200

    init

--- animrec/__init__.py
+++ animrec/__init__.py
@@ -1,2 +1,6 @@
-from animrec.animrec import *
-from animrec.version import __version__
+from .animrec import *
+print "animrec done"
+from .getedge import take_img
+print "getedge done"
+from .version import __version__
+print "version done"
