66ebebe89f99ecb89a7baff1f8fb47a7e0b0f104
joljols <j.w.jolles@gmail.com>
2018-08-28 12:13:47 +0200

    setup file

--- animrec/__init__.py
+++ animrec/__init__.py
@@ -1,6 +1,2 @@
-#from .animrec import *
-#print "animrec done"
-#from .getedge import take_img
-#print "getedge done"
-#from .version import __version__
-#print "version done"
+from animrec import *
+from .version import __version__
