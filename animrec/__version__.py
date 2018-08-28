d2a0b9be9ff8c514664e19a9d973d179e80802cf
joljols <j.w.jolles@gmail.com>
2018-08-28 14:38:08 +0200

    version

--- animrec/__version__.py
+++ animrec/__version__.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 
+#28/08/18 - 1.0.5   Added complete, detailed Readme file with guide and workflow
 #28/08/18 - 1.0.4   Fixed init script executing functions
 #28/08/18 - 1.0.3   Create AnimRec logo
 #28/08/18 - 1.0.2   Updated setup file with print statements regarding dependencies
