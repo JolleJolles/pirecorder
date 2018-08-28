a7700ffcc0645b01ecd2a6d3a9af8908fc1bd00d
joljols <j.w.jolles@gmail.com>
2018-08-28 12:07:59 +0200

    asdasdasd

--- animrec/__init__.py
+++ animrec/__init__.py
@@ -1,6 +1,6 @@
 #from .animrec import *
-print "animrec done"
-from .getedge import take_img
-print "getedge done"
-from .version import __version__
-print "version done"
+#print "animrec done"
+#from .getedge import take_img
+#print "getedge done"
+#from .version import __version__
+#print "version done"
