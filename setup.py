77c8f5a96d73fda0dcb8a6502c38f11fa77bc821
Jolle Jolles <jollejolles@Jolles-MBP.fritz.box>
2018-12-17 22:15:53 +0100

    Add apache license 2.0

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 #! /usr/bin/env python
 #
-# Copyright (C) 2015-2018 Jolle Jolles <j.w.jolles@gmail.com>
+# Controlled media recording library for the Rasperry-Pi
+# Copyright (c) 2018 Jolle Jolles <j.w.jolles@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from __future__ import print_function
 from setuptools import setup
-from animrec.__version__ import __version__
+from animrec.__init__ import __version__
 
 
-DESCRIPTION = 'AnimRec: controlled media recording with the RPi'
+DESCRIPTION = 'AnimRec: Controlled media recording library for the Rasperry-Pi'
 LONG_DESCRIPTION = """\
 AnimRec is a python package designed to help facilitate automated
 recording using the RPi, specifically with easy, customized, repeated
@@ -18,10 +31,6 @@ def check_dependencies():
     install_requires = []
 
     # Make sure dependencies exist
-#    try:
-#        import picamera
-#    except ImportError:
-#        install_requires.append('picamera')
     try:
         import pathos
     except ImportError:
