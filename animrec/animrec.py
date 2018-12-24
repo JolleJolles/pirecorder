64a4225ed1664aa7dae0d355084998d67ddf5a83
joljols <j.w.jolles@gmail.com>
2018-12-24 13:55:00 +0100

    fix

--- animrec/animrec.py
+++ animrec/animrec.py
@@ -420,7 +420,7 @@ class Recorder:
                 output.seek(0)
                 output.truncate()
 
-        self.set_config(gains=self.cam.awb_gains, internal="")
+        self.set_config(gains="(R:%5.2f, B:%5.2f)", internal="")
         alu.lineprint("Gains: " + "(R:%5.2f, B:%5.2f)" % (rg, bg) + " stored..")
         self.cam.close()
 
