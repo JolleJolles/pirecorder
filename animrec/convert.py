75334c454e0c48694c253fe5a949967ce1128aa8
joljols <j.w.jolles@gmail.com>
2018-12-28 14:17:41 +0100

    Improved layout and doc

--- animrec/convert.py
+++ animrec/convert.py
@@ -15,7 +15,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from six.moves import input
 from __future__ import print_function
+
+from .__version__ import __version__
+import animlab.utils as alu
+import animlab.imutils as alimu
 
 import os
 import cv2
@@ -26,10 +31,6 @@ import subprocess
 
 from datetime import datetime
 from pathos.multiprocessing import ProcessPool
-from six.moves import input
-
-import animlab.utils as alu
-import animlab.imutils as alimu
 
 
 class KeyboardInterruptError(Exception):
@@ -39,38 +40,38 @@ class Converter:
 
     """ Initializes a converter instance """
 
-    def __init__(self, dir_in="", vidtype=".h264", conv_type="standard",
-                  remove=False, pools=6, resizeval=1, displayframenr = 100):
+    def __init__(self, dir_in = "", vidtype = ".h264", conv_type = "standard",
+                 remove = False, pools = 6, resizeval = 1, displayframenr = 100):
 
-        '''
+        """
         Converter class to convert a directory of videos to mp4 with
         potential to write frame number on each frames
 
         Parameters
         -----------
-        dir_in : str, default =""
-            Directory containing the videos
-        vidtype : str, default=""
+        dir_in : str, default = ""
+            Directory containing the videos.
+        vidtype : str, default = ""
             The filetype of the video to convert
-        convtype : ["standard","withframe"], default = "standard"
+        convtype : ["standard", "withframe"], default = "standard"
             Type of conversion, either very fast conversion using ffmpeg or
-            using opencv to add frame number to each frame
-        remove :  boolean, default=False
-            If the original videos should be removed or not
-        pools : int, default=6
-            Number of cores to use for conversion
-        resizeval : float, default=1,
-            Float value to which video should be resized
-        displayframenr : int, default="100"
-            Interval at which frame nr should be displayed during conversion
-        '''
+            using opencv to add frame number to each frame.
+        remove :  boolean, default = False
+            If the original videos should be removed or not.
+        pools : int, default = 6
+            Number of computer cores to use for conversion script.
+        resizeval : float, default = 1
+            Float value to which video should be resized.
+        displayframenr : int, default = 100
+            Interval at which frame nr should be displayed during conversion.
+        """
 
         alu.lineprint("Convert function started!", label = "AnimRec")
 
         maindir = os.getcwd()
         dir_out = dir_in if dir_out == "" else dir_out
-        assert os.path.exists(dir_in), "dir_in directory does not exist, try again"
-        assert os.path.exists(dir_out), "dir_out directory does not exist, try again"
+        assert os.path.exists(dir_in), "dir_in does not exist, try again.."
+        assert os.path.exists(dir_out), "dir_out does not exist, try again.."
         self.dir_in = dir_in
 
         self.vidtype = vidtype
@@ -88,24 +89,29 @@ class Converter:
 
     def conv_ffmpeg(self):
 
+        """ Runs convert command with ffmpeg"""
+
         for filein in self.conv_files:
             if self.resizeval != 1:
-                command = "' -vf 'scale=iw*"+str(self.resizeval)+":ih*"+str(self.resizeval)+"' '"
+                comm = "' -vf 'scale=iw*" + str(self.resizeval) + ":ih*" +\
+                comm = comm + str(self.resizeval) + "' '"
             else:
-                command = "' -vcodec copy '"
-            bashCommand = "ffmpeg -i '" + filein + command + filein[:-5] + ".mp4' -y"
-            output = subprocess.check_output(['bash','-c', bashCommand])
-            alu.lineprint("Finished converting "+filein, label = "AnimRec")
+                comm = "' -vcodec copy '"
+            bashcomm = "ffmpeg -i '" + filein + comm + filein[:-5] + ".mp4' -y"
+            output = subprocess.check_output(['bash','-c', bashcomm])
+            alu.lineprint("Finished converting " + filein, label = "AnimRec")
 
         self.done = True
 
 
     def conv_cv(self):
 
+        """ Sets up running of convert command with opencv"""
+
         if self.pools == 1:
             for filein in self.conv_files:
                 self.conv_cvfile(filein)
-            self.done = "True"
+            self.done = True
 
         else:
             pool = ProcessPool(self.pools)
@@ -115,17 +121,20 @@ class Converter:
                 self.done = True
                 alu.lineprint("Converting done!", label = "AnimRec")
             except KeyboardInterrupt:
-                alu.lineprint("Got ^C, terminating pool converting", label = "AnimRec")
+                alu.lineprint("Got ^C, terminating pool", label = "AnimRec")
                 pool.terminate()
             except Exception as e:
-                alu.lineprint("got exception: %r, terminating pool converting" % (e,), label = "AnimRec")
+                excep = "Got exception: %r, terminating pool" % (e,)
+                alu.lineprint(excep, label = "AnimRec")
                 pool.terminate()
 
 
     def conv_cvfile(self, filein):
 
+        """ Converts single file with opencv"""
+
         try:
-            alu.lineprint("Start converting "+filein, label = "AnimRec")
+            alu.lineprint("Start converting " + filein, label = "AnimRec")
 
             vid = cv2.VideoCapture(filein)
             fps,width,height,_ = alimu.get_vid_params(vid)
@@ -140,11 +149,11 @@ class Converter:
                         if frame_nr % self.displayframenr == 0:
                             print(str(frame_nr), end=" ")
 
-                    cv2.putText(frame,str(frame_nr),(10,35),cv2.FONT_HERSHEY_SIMPLEX,0.9,(0,0,0),2)
+                    alimu.draw_text(frame, str(frame_nr), (10,35), 0.9)
                     vidout.write(frame)
 
                 if not flag:
-                    alu.lineprint("Finished converting "+filein, label = "AnimRec")
+                    alu.lineprint("Finished converting " + filein, label = "AnimRec")
                     break
 
         except KeyboardInterrupt:
@@ -162,7 +171,7 @@ class Converter:
             if self.done and self.remove:
                 for filein in self.conv_files:
                     os.remove(filein)
-                alu.lineprint("Removed all original files", label = "AnimRec")
+                alu.lineprint("Removed all original files..", label = "AnimRec")
 
         else:
-            alu.lineprint("no files found...", label = "AnimRec")
+            alu.lineprint("no files found..", label = "AnimRec")
