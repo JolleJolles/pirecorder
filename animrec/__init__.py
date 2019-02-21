7a27b5ec6eb42f347e65d2a5e37ecaf4d38ad8e5
Jolle Jolles <jollejolles@Jolles-MBP-3.fritz.box>
2019-02-21 15:27:43 +0100

    1.1.2

--- animrec/__init__.py
+++ animrec/__init__.py
@@ -1,3 +1,11 @@
+"""
+AnimRec is a python package build to help facilitate automated recording using
+the RPi, with easy, customized, repeated image and video recording. It is
+specifically designed with the Behavioural Scientist in mind and contains
+documentation and tutorials to be installable and easy to use for people with
+limited knowledge of coding.
+"""
+
 #! /usr/bin/env python
 #
 # Controlled media recording library for the Rasperry-Pi
@@ -14,14 +22,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""
-AnimRec is a python package build to help facilitate automated recording using
-the RPi, with easy, customized, repeated image and video recording. It is
-specifically designed with the Behavioural Scientist in mind and contains
-documentation and tutorials to be installable and easy to use for people with
-limited knowledge of coding.
-"""
 
 from .__version__ import __version__
 from .animrec import *
--- animrec/__version__.py
+++ animrec/__version__.py
@@ -15,6 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 # For documentation of all changes, see CHANGELOG
