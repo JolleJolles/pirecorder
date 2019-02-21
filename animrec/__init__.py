31d643c238645289e11ba4e0326d0c2586f3d05b
Jolle Jolles <jollejolles@Jolles-MBP-3.fritz.box>
2019-02-21 15:14:27 +0100

    Module documentation added

--- animrec/__init__.py
+++ animrec/__init__.py
@@ -15,5 +15,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+AnimRec is a python package build to help facilitate automated recording using
+the RPi, with easy, customized, repeated image and video recording. It is
+specifically designed with the Behavioural Scientist in mind and contains
+documentation and tutorials to be installable and easy to use for people with
+limited knowledge of coding.
+"""
+
 from .__version__ import __version__
 from .animrec import *
