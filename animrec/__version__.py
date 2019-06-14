ec3e44f90f395306fa012e0886ed38e00e76e7b4
Jolle Jolles <j.w.jolles@gmail.com>
2019-06-14 12:40:27 +0200

    1.3.2

--- animrec/__version__.py
+++ animrec/__version__.py
@@ -15,6 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "1.3.0"
+__version__ = "1.3.2"
 
 # For documentation of all changes, see CHANGELOG
