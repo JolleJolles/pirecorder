fb9aa76f5c2fa0d0c4167d75e1f3ff30324bb8e6
Jolle Jolles <jollejolles@Jolles-MacBook-Pro-3.local>
2019-06-18 10:33:21 +0200

    stream_video script

--- scripts/stream_video.py
+++ scripts/stream_video.py
@@ -1,23 +1,29 @@
+#! /usr/bin/env python
+
 # import opencv and the Videostream package
 import cv2
 from animrec.videoin import VideoIn
 
-# Open a video stream: cam = "rpi" will open the native raspberry
-# pi camera, while cam = 0 will open the natively attached camera or webcam
-vid = VideoIn(resolution=(1920, 1080)).start()
+def stream_video():
+    # Open a video stream: cam = "rpi" will open the native raspberry
+    # pi camera, while cam = 0 will open the natively attached camera or webcam
+    vid = VideoIn(resolution=(1920, 1080)).start()
 
-# Run videostream until user presses ESC
-while True:
+    # Run videostream until user presses ESC
+    while True:
 
-    # Get frame from the videostream
-    frame = vid.read()
+        # Get frame from the videostream
+        frame = vid.read()
 
-    # Show the frame
-    cv2.imshow('window', frame)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        # Show the frame
+        cv2.imshow('window', frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
-# Stop the videostream and close the video window
-vid.stop()
-cv2.destroyAllWindows()
-cv2.waitKey(1)
+    # Stop the videostream and close the video window
+    vid.stop()
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+
+if __name__ == "__main__":
+      stream_video()
