611a842072fd4948da1bbecc4298e7364d2d9aef
Jolle Jolles <jollejolles@Jolles-MacBook-Pro-3.local>
2019-06-18 09:45:38 +0200

    Updated VideoIn class name

--- scripts/stream_video.py
+++ scripts/stream_video.py
@@ -1,16 +1,16 @@
 # import opencv and the Videostream package
 import cv2
-from animrec.videostream import VideoStream
+from animrec.videoin import VideoIn
 
 # Open a video stream: cam = "rpi" will open the native raspberry
 # pi camera, while cam = 0 will open the natively attached camera or webcam
-vidstream = VideoStream(resolution=(1920, 1080)).start()
+vid = VideoIn(resolution=(1920, 1080)).start()
 
 # Run videostream until user presses ESC
 while True:
 
     # Get frame from the videostream
-    frame = vidstream.read()
+    frame = vid.read()
 
     # Show the frame
     cv2.imshow('window', frame)
@@ -18,6 +18,6 @@ while True:
         break
 
 # Stop the videostream and close the video window
-vidstream.stop()
+vid.stop()
 cv2.destroyAllWindows()
 cv2.waitKey(1)
