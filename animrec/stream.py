8aa46d000dd6605448918af4f44b0eb702211b09
joljols <j.w.jolles@gmail.com>
2019-11-07 20:12:47 +0100

    added doc and removed comments

--- animrec/stream.py
+++ animrec/stream.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 """
-Controlled media recording library for the Rasperry-Pi
-Copyright (c) 2019 Jolle Jolles <j.w.jolles@gmail.com>
+Copyright (c) 2015 - 2019 Jolle Jolles <j.w.jolles@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,35 +15,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-# Import opencv and the Videostream package
 import cv2
+
 from animrec.videoin import VideoIn
 
 def stream():
-    # Open a video stream: cam = "rpi" will open the native raspberry
-    # pi camera, while cam = 0 will open the natively attached camera or webcam
+
+    """
+    Opens a video stream of the rpi camera. With keypress 'f' the user can make
+    the videostream fullscreen and with the 'ESC' key the window is closed.
+    """
+
     vid = VideoIn(vidsize=0.25).start()
-
-    # Run videostream until user presses ESC
     while True:
-
-        # Get frame from the videostream
         frame = vid.read()
-
-        # Show the frame
         cv2.imshow('window', frame)
         k = cv2.waitKey(1) & 0xFF
-
-        # Change to/from fullscreen
         if k == ord("f"):
             winval = abs(1 - cv2.getWindowProperty('window', 0))
             cv2.setWindowProperty("window", 0, winval)
-
-        # Exit
         if k == 27:
             break
 
-    # Stop the videostream and close the video window
     vid.stop()
     cv2.destroyAllWindows()
     cv2.waitKey(1)
