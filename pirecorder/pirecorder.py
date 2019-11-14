6370ae1b3ac49124a7e9676336388ca3daef7b41
Jolle Jolles <j.w.jolles@gmail.com>
2019-11-14 15:54:32 +0100

    ASDNASIDJASOIDJSOIDJ

--- pirecorder/pirecorder.py
+++ pirecorder/pirecorder.py
@@ -429,7 +429,7 @@ class Recorder:
         self._setup_cam()
         self._namefile()
 
-        self.cam.start_recording("sinterklaas", quality = self.config.vid.vidquality)
+        self.cam.start_recording("sinterklaas.h264", quality = self.config.vid.vidquality)
         lineprint("Recording YEAH ")
         self.cam.wait_recording(self.config.vid.vidduration + self.config.vid.viddelay)
         self.cam.stop_recording()
