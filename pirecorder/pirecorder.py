6f79b12a40fd4f80e8d16570b8ef73bfde7383b6
Jolle Jolles <j.w.jolles@gmail.com>
2019-12-23 17:11:37 +0100

    auto get_gains

--- pirecorder/pirecorder.py
+++ pirecorder/pirecorder.py
@@ -422,7 +422,8 @@ class PiRecorder:
         """Automatically finds the best gains for the raspberry pi camera"""
 
         (rg, bg) = getgains(startgains = checkfrac(self.config.cus.gains),
-                            zoom = literal_eval(self.config.cus.roi))
+                            zoom = literal_eval(self.config.cus.roi),
+                            auto = auto)
         self.set_config(gains="(%5.2f, %5.2f)" % (rg, bg), internal="")
         lineprint("Gains: " + "(R:%5.2f, B:%5.2f)" % (rg, bg) + " stored..")
 
